cd230f9a6a0e4aa72415b9ef006850601b0d54e4
Jeff Minelli <jgm106@psu.edu>
2014-09-16 15:22:09 -0400

    props. take 9

--- extension.js
+++ extension.js
@@ -160,9 +160,7 @@
                 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                 if (!bot.commands.executable(this.rank, chat)) return void (0);
                 else {
-                  API.sendChat(basicBot.userUtilities.lookupUserName(chat.id).username +" just gave props to @"+ API.getDJ().username +" for playing a great track!"); // returns "not defined"
-                  //API.sendChat(basicBot.userUtilities.lookupUserName(id).username +" just gave props to @"+ API.getDJ().username +" for playing a great track!"); // returns "not defined"
-                  //API.sendChat(this.username +" just gave props to @"+ API.getDJ().username +" for playing a great track!"); // this.username returns undefined.
+                  API.sendChat(chat.un +" just gave props to @"+ API.getDJ().username +" for playing a great track!");
                 }
             }
         }; // props
