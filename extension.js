80ff7a0ee35d7d7420ca207d022831ec424d9a34
RogieRog <roger.settle@gmail.com>
2015-04-28 14:49:42 -0400

    Update extension.js

--- extension.js
+++ extension.js
@@ -362,7 +362,7 @@
                 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                 if (!bot.commands.executable(this.rank, chat)) return void (0);
                 else {
-                    var quotes = [ "Utinni! http://goo.gl/VcSi6l", "RogieRog is the greatest!", "Have I ever told you how awesome RogieRog is?", "Roger, Roger. Whats our vector, Victor?" ];
+                    var quotes = [ "RogieRog IN THE HOUSE!", "Roger is the greatest!", "Have I ever told you how awesome Roger is?", "Roger, Roger. Whats our vector, Victor?" ];
                     API.sendChat("/me " + quotes[Math.floor(Math.random() * quotes.length)]);
                 }
             }
