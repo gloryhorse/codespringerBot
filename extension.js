ed8619bdd92aa83372a8fcb859eed15e5f62603b
RogieRog <roger.settle@gmail.com>
2015-04-28 14:17:31 -0400

    Update extension.js

--- extension.js
+++ extension.js
@@ -44,6 +44,19 @@
                 }
             }
         }; // bacon
+        
+                bot.commands.bigCommand = {
+            command: 'big',
+            rank: 'user',
+            type: 'exact',
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("What would you like me to say?");
+                }
+            }
+        }; // big
 
         bot.commands.banjoCommand = {
             command: 'banjo',
