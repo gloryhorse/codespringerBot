1f46d821db9f93fcd1e3562f122dc2a9997de1da
RogieRog <roger.settle@gmail.com>
2015-04-30 12:35:27 -0400

    Update extension.js

--- extension.js
+++ extension.js
@@ -53,7 +53,7 @@
                 if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                 if (!bot.commands.executable(this.rank, chat)) return void (0);
                 else {
-                    API.sendChat("This boys way off the charts, as far as I can tell - Ohhhh Weeeee momma hes a double XL!");
+                    API.sendChat("What would you like me to say?");
                 }
             }
         }; // big
