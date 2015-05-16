21d2a034612733edcc64c63a948bad6814e8bf09
gloryhorse <glorypam@gmail.com>
2015-05-16 06:14:18 -0800

    Update extension.js

--- extension.js
+++ extension.js
@@ -1713,7 +1713,7 @@
 
             commandsCommand: {
                 command: 'commands',
-                rank: 'user',
+                rank: 'bouncer',
                 type: 'exact',
                 functionality: function (chat, cmd) {
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
