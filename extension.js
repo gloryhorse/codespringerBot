9425e7c4e249025d85843cdb64f8032e4fce4209
gloryhorse <glorypam@gmail.com>
2015-05-16 19:50:13 -0800

    Update extension.js

--- extension.js
+++ extension.js
@@ -178,9 +178,9 @@
     var botMaintainer = "Benzi (Quoona)"
     var botCreatorIDs = ["3851534", "4105209"];
 
-    var basicBot + {
-        status: true,
-        version: 1.1
+    var basicBot = {
+    	version: 1.1
+        status: false,
         name: "CodeSpringerBot",
         loggedInID: null,
         scriptLink: "https://rawgit.com/Yemasthui/basicBot/master/basicBot.js",
